bde5604261a95f74b5dfd4951a940833ee3f1342
Garnett05 <charlleslimamota10@hotmail.com>
2020-03-25 22:05:48 -0300

    Finalização da sequência de fetuccine. A sequência é inserida em 3 tipos de matrizes: normal, oposta e transposta

--- main.c
+++ main.c
@@ -8,20 +8,17 @@ Desenvolvido por Charlles Lima
 
 int main(void) {
 
-int n1, n2, aux, c;
+int aux, c, s, qntd;
 printf("How many elements will the sequence have? ");
-scanf("%i", &aux);
+scanf("%i", &qntd);
+int fetuc[qntd];
 printf("First element of the sequence: ");
-scanf("%i", &n1);
-printf("Second element of the sequence: ");
-scanf("%i", &n2);
-
-int fetuc[aux];
-fetuc[0] = n1;
-fetuc[1] = n2;
+scanf("%i", &fetuc[0]);
+printf("Second element of the sequence: \n");
+scanf("%i", &fetuc[1]);
 
 c = 2;
-while (c < aux){
+while (c < qntd){ //Neste laço, estao sendo inseridos os demais elementos  da série no vetor fetuc
   if (c % 2 == 0){
     fetuc[c] = fetuc[c-1] + fetuc[c-2];
   }
@@ -30,6 +27,46 @@ while (c < aux){
   }
   c++;
 }
+
+aux = 1;
+  while (aux*aux < qntd){ //Laço para determinar o tamanho da matriz quadrada
+    aux++;
+  }
+  int mat [aux][aux];
+  s = 0;
+  for (int i = 0; i < aux; i++){    
+    for (int j = 0; j < aux; j++){
+      if (s < qntd){
+        mat[i][j] = fetuc[s];
+      }
+      else{
+        mat[i][j] = 0;
+      }
+      s++;
+    }
+  }  
+
+  printf("Matriz no formato normal:");
+  for (int i = 0; i < aux; i++){
+      printf("\n");
+      for (int j = 0; j < aux; j++){
+       printf("%i ", mat[i][j]);
+    }
+  }
+  printf("\n\nMatriz no formato oposto:");
+  for (int i = 0; i < aux; i++){
+      printf("\n");
+      for (int j = 0; j < aux; j++){
+       printf("%i ", mat[i][j] * -1);
+    }
+  }
+  printf("\n\nMatriz no formato transposto:");
+  for (int i = 0; i < aux; i++){
+      printf("\n");
+      for (int j = 0; j < aux; j++){
+       printf("%i ", mat[j][i]);
+    }
+  }
 
   return 0;
 }
